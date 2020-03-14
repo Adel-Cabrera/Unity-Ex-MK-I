bbd0851909e57d224272b1fc2f27baade2757635
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-03-14 00:32:06 -0300

    First project with Unity & C#, practicing classes, data types, libraries and variables

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int hitPoints = 20;
+    float speed = 3.8f;
+    bool isAlive = true;
+    string name = "Rick";
+    int max;
+    int min;
+
+    void Start()
+    {
+        max = 1000;
+        min = 1;
+        Debug.Log("Welcome to number wizard, yo");
+        Debug.Log("Pick a number between " + min + " and " + max);
+        Debug.Log(hitPoints);
+        Debug.Log(speed);
+        Debug.Log(isAlive);
+        isAlive = false;
+        Debug.Log(isAlive);
+        Debug.Log(name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
