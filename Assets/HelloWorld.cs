11acfcd45211628f88a043762abff297d8e83a5a
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-03-14 01:46:36 -0300

    Finished first excercise with void functions

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -11,11 +11,19 @@ public class HelloWorld : MonoBehaviour
     string name = "Rick";
     int max;
     int min;
+    int guess;
+
 
     void Start()
     {
+        StartGame();
+    }
+
+    void StartGame() {
+
         max = 1000;
         min = 1;
+        guess = 500;
         Debug.Log("Welcome to number wizard, yo");
         Debug.Log("Pick a number between " + min + " and " + max);
         Debug.Log(hitPoints);
@@ -24,11 +32,42 @@ public class HelloWorld : MonoBehaviour
         isAlive = false;
         Debug.Log(isAlive);
         Debug.Log(name);
+        Debug.Log("Tell me if your number is higher or lower than " + guess);
+        Debug.Log("Up arrow = higher | Low arrow = lower. Press ENTER = correct");
+        max = max + 1;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow");
+            min = guess;
+            NextGuess();
+
+        }
+
+        else if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down Arrow");
+            max = guess;
+            NextGuess();
+
+        }
+
+        else if(Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Enter");
+            StartGame();
+        }
         
+    }
+
+    void NextGuess(){
+        guess = (max + min / 2);
+        Debug.Log("H or L than " + guess);
+
     }
 }
